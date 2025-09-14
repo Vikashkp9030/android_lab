cb0a53c6ad7730202e36f2ae9d1c1da620a8dac4
Vikashkp9030 <vikash798561@gmail.com>
2025-09-14 23:41:20 +0530

    day 2

--- app/src/main/java/com/example/android_lab/days/day2.kt
+++ app/src/main/java/com/example/android_lab/days/day2.kt
@@ -1,5 +1,35 @@
 fun main(){
+    listFun()
     arrayFun()
+    println("Vikash".addHello())
+    println(10.square())
+}
+
+fun String.addHello():String{
+    return "Hello, $this"
+}
+fun Int.square(): Int = this*this;
+
+fun listFun(){
+    val list1 = listOf<Int>(1,3,5,6,7)
+    val doubled = list1.map { it*2}
+    val filtered = list1.map { it%2==0 }
+    println(list1.reduce{acc,i -> acc+i})
+    val list2 = mutableListOf<String>("Apple","Mango")
+    val set = setOf<Int>(3,4,55,6,3,4,5)
+    for(d in set.toList()){
+        println(d)
+    }
+    list2.add("Orange")
+    list2.forEach {
+        println(it)
+    }
+    for(d in list1){
+        println(d)
+    }
+    for ((i,value ) in list1.withIndex()){
+        println(list1.get(i))
+    }
 }
 fun  arrayFun(){
     val arr1 = arrayOf(1,2,3)
